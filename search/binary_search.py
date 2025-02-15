3d6b710bab94d9c069c74711379cd5848c74105c
sw1203 <tkddnjs1203@gmail.com>
2025-02-16 00:23:25 +0900

    fix: 참고링크 추가

--- search/binary_search.py
+++ search/binary_search.py
@@ -4,6 +4,8 @@
 - 배열의 중간 값과 찾으려는 값을 비교하여, 중간 값이 찾으려는 값보다 크면 왼쪽을, 작으면 오른쪽을 탐색
 - 시간 복잡도: O(logN)
 - return: 찾으려는 값의 인덱스 (없으면 None)
+- python library: bisect
+    - https://dduniverse.tistory.com/entry/python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9D%B4%EC%A7%84-%ED%83%90%EC%83%89-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-bisect-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
 """
 def bianry_search(arr, target, low=0, high=None):
     if high is None:
